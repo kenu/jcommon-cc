dd9be268742a24aa4685257e6b10275d9a703158
hbenicloud <hbenicloud@gmail.com>
2023-11-03 20:28:57 +0900

    Hbenicloud (#1)
    * refactor if
    
    * comment 수정, 주석 처리된 코드 삭제

--- src/main/java/org/jfree/chart/ChartTheme.java
+++ src/main/java/org/jfree/chart/ChartTheme.java
@@ -40,12 +40,10 @@
 
 package org.jfree.chart;
 
-import org.jfree.chart.JFreeChart;
-
 /**
  * A {@link ChartTheme} a class that can apply a style or 'theme' to a chart.
  * It can be implemented in an arbitrary manner, with the styling applied to
- * the chart via the <code>apply(JFreeChart)</code> method.  We provide one
+ * the chart via the <code>apply(JFreeChart)</code> method. We provide one
  * implementation ({@link StandardChartTheme}) that just mimics the manual
  * process of calling methods to set various chart parameters.
  *
@@ -56,7 +54,7 @@ public interface ChartTheme {
     /**
      * Applies this theme to the supplied chart.
      *
-     * @param chart  the chart (<code>null</code> not permitted).
+     * @param chart the chart (<code>null</code> not permitted).
      */
     public void apply(JFreeChart chart);
 
